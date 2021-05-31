01f2076f7b8c114c21182fbfd7d623f09ebced89
Max Cahill <max@1bardesign.com>
2021-05-31 20:41:32 +1000

    basic example

---
+++ conf.lua
@@ -0,0 +1,6 @@
+function love.conf(t)
+	--use desktop dimensions
+	t.window.width = 0
+	t.window.height = 0
+	t.window.fullscreen = true
+end
--- main.lua
+++ main.lua
@@ -1,17 +1,80 @@
 require("lib.batteries"):export()
 
-love.window.setMode(1,2)
+local font_size = 80
+love.graphics.setFont(love.graphics.newFont(font_size))
+love.keyboard.setKeyRepeat(true)
 
 function love.touchpressed()
+	--touched the screen? open the keyboard
 	love.keyboard.setTextInput(true)
 end
 
-local draw_text = "test text"
+local prompt_test = "lets enter some text"
+local response_text = ""
+local response_limit = 30
+local caret_period = 1.3
+
+function execute()
+	--handle response here
+	if response_text == "exit" then
+		love.event.quit()
+	elseif response_text == "win" then
+		prompt_test = "great job"
+	end
+	response_text = ""
+end
+
+local utf8 = require("utf8")
 
 function love.textinput(t)
-	draw_text = table.concat({draw_text, t}, "")
+	response_text = response_text .. t
+	while utf8.len(response_text) > response_limit do
+		response_text = response_text:sub(1, utf8.offset(response_text, response_limit + 1) - 1)
+	end
+	if t:find("\n") ~= nil then
+		execute()
+	end
+end
+
+function love.keypressed(k)
+	if k == "backspace" and #response_text > 0 then
+		response_text = response_text:sub(1, utf8.offset(response_text, -1) - 1)
+	end
+
+	if k == "return" then
+		execute()
+	end
+
+	--dev conveniences
+	if k == "escape" then
+		love.event.quit()
+	end
+	if love.keyboard.isDown("lctrl", "rctrl") then
+		if k == "q" then
+			love.event.quit()
+		elseif k == "r" then
+			love.event.quit("restart")
+		end
+	end
 end
 
 function love.draw()
-	love.graphics.print(draw_text,10,10)
+	local w, h = love.graphics.getDimensions()
+	love.graphics.translate(
+		w / 2,
+		h / 2
+	)
+	local caret = (math.wrap(love.timer.getTime(), 0, caret_period) < caret_period * 0.5 and "" or "|")
+	--wiggle
+	love.graphics.rotate(math.sin(love.timer.getTime() * 0.6 * math.tau) * 0.01)
+	for i, v in ipairs({
+		prompt_test,
+		"> " ..response_text .. caret
+	}) do
+		love.graphics.print(
+			v,
+			-w / 3,
+			(i - 2) * font_size * 1.5
+		)
+	end
 end
